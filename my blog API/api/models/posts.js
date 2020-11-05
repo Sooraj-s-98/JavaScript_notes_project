1068a31b3154cc744e02fa67e30039160dd21ccb
sooraj <sooraj1998ayiroor@gmail.com>
2020-11-05 13:42:19 +0530

    get individual blog by id

--- my blog API/api/models/posts.js
+++ my blog API/api/models/posts.js
@@ -3,7 +3,16 @@ const fs=require('fs');
 class post {
 
     add(newPost) {
+        const currentPosts=this.readData();
+        currentPosts.unshift(newPost);
+        this.storeData(newPost);
 
+    }
+
+    getIndividualBlog(postId){
+        const posts=this.readData();
+        const foundPost=posts.find((post)=>post.id==postId);
+        return foundPost;
     }
 
     get() {
@@ -15,7 +24,10 @@ class post {
         let posts=JSON.parse(rawdta);
         return posts;
     }
-
+    storeData(){
+        let data=Json.stringfy(rawdta);
+        fs.writeFileSync(PATH,data)
+    }
 }
 
 module.exports = post;
